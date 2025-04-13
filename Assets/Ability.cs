0136b191ad6c334a78b8c05e3057f466b090d24e
Башков Михаил Антонович <141922582+rd238@users.noreply.github.com>
2025-04-13 11:08:16 +0300

    Добавлены ability

--- Assets/Ability.cs
+++ Assets/Ability.cs
@@ -5,3 +5,82 @@ public class AbilityAbstract
     virtual void useAbility() = 0;
 }
 
+public class AbilityWithout : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilitySurgeOfSthreght : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilityCommandersHorn : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilityDouble : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilityHero : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilityStrongConnection : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilitySpy : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilityHaze : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilityAxe : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilityMedic : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilityFreezing : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilityRain : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilityScarecrow : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilityAx : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilityClearSkies : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
+
+public class AbilityNoEffect : AbilityAbstract
+{
+    override void useAbility() { return; };
+}
