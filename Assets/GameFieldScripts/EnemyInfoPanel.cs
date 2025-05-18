93f149357ca43e10069d223fe9ec6ed54363849b
McLaren95 <maksimkruglikov1@gmail.com>
2025-05-18 19:14:16 +0300

    Название фракций переходит в игровое поле

--- Assets/GameFieldScripts/EnemyInfoPanel.cs
+++ Assets/GameFieldScripts/EnemyInfoPanel.cs
@@ -1,6 +1,57 @@
 using UnityEngine;
+using TMPro;
 
-public class EnemyInfoPanel
+public class EnemyInfoPanel : MonoBehaviour
 {
-    
+    void Start()
+    {
+        GameObject enemyObject = GameObject.Find("player_ciri");
+
+        if (enemyObject != null)
+        {
+            Player enemy = enemyObject.GetComponent<Player>();
+
+            if (enemy != null)
+            {
+                string enemyFactionName = enemy.name_fraction;
+
+                GameObject enemyInfoPanel = GameObject.Find("EnemyInfoPanel");
+
+                if (enemyInfoPanel != null)
+                {
+                    Transform factionNameTransform = enemyInfoPanel.transform.Find("EnemyFactionName");
+
+                    if (factionNameTransform != null)
+                    {
+                        TMP_Text textMeshPro = factionNameTransform.GetComponent<TMP_Text>();
+
+                        if (textMeshPro != null)
+                        {
+                            textMeshPro.text = enemyFactionName;
+                        }
+                        else
+                        {
+                            Debug.LogError("TextMeshPro component not found on EnemyFactionName.");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError("EnemyFactionName object not found inside EnemyInfoPanel.");
+                    }
+                }
+                else
+                {
+                    Debug.LogError("EnemyInfoPanel object not found in the scene.");
+                }
+            }
+            else
+            {
+                Debug.LogError("Player component not found on player_ciri.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Player object 'player_ciri' not found.");
+        }
+    }
 }
--- Assets/GameFieldScripts/PlayerInfoPanel.cs
+++ Assets/GameFieldScripts/PlayerInfoPanel.cs
@@ -1,6 +1,57 @@
 using UnityEngine;
+using TMPro;
 
-public class PlayerInfoPanel
+public class PlayerInfoPanel : MonoBehaviour
 {
-    
+    void Start()
+    {
+        GameObject playerObject = GameObject.Find("player_geralt");
+
+        if (playerObject != null)
+        {
+            Player player = playerObject.GetComponent<Player>();
+
+            if (player != null)
+            {
+                string playerFactionName = player.name_fraction;
+
+                GameObject playerInfoPanel = GameObject.Find("PlayerInfoPanel");
+
+                if (playerInfoPanel != null)
+                {
+                    Transform factionNameTransform = playerInfoPanel.transform.Find("FactionName");
+
+                    if (factionNameTransform != null)
+                    {
+                        TMP_Text textMeshPro = factionNameTransform.GetComponent<TMP_Text>();
+
+                        if (textMeshPro != null)
+                        {
+                            textMeshPro.text = playerFactionName;
+                        }
+                        else
+                        {
+                            Debug.LogError("TextMeshPro component not found on FactionName.");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError("FactionName object not found inside PlayerInfoPanel.");
+                    }
+                }
+                else
+                {
+                    Debug.LogError("PlayerInfoPanel object not found in the scene.");
+                }
+            }
+            else
+            {
+                Debug.LogError("Player component not found on player_geralt.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Player object 'player_geralt' not found.");
+        }
+    }
 }
