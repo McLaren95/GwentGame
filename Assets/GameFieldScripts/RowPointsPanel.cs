fa7ff6fd14f1794e0c42ccab2b11fdbd674c68d4
McLaren95 <maksimkruglikov1@gmail.com>
2025-05-24 14:27:11 +0300

    Created RowPointsPanel Script

---
+++ Assets/GameFieldScripts/RowPointsPanel.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using TMPro;
+
+public class RowPointsPanel : MonoBehaviour
+{
+    public TMP_Text playerMeleePointsText;
+    public TMP_Text playerRangedPointsText;
+    public TMP_Text playerSiegePointsText;
+    public TMP_Text enemyMeleePointsText;
+    public TMP_Text enemyRangedPointsText;
+    public TMP_Text enemySiegePointsText;
+    public GwentRound gwentRound;
+
+
+    void Start()
+    {
+
+    }
+
+    public void set_round(GwentRound round)
+    {
+
+    }
+}
