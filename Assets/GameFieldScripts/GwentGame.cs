d6c070e2ec796e3dfe3a75815f3a6d364fe40de2
Башков Михаил Антонович <141922582+rd238@users.noreply.github.com>
2025-05-24 16:47:18 +0300

    окончание игры

--- Assets/GameFieldScripts/GwentGame.cs
+++ Assets/GameFieldScripts/GwentGame.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
+using TMPro;
+
 
 public class GwentGame : MonoBehaviour
 {
@@ -8,8 +13,71 @@ public class GwentGame : MonoBehaviour
 
     public RowPointsPanel panel;
 
+    public List<GameObject> healt_player;
+    public List<GameObject> healt_enemy;
+
     Player winner;
 
+    private void init_health()
+    {
+        healt_player.Add(GameObject.Find("PlayerHealthIcon1"));
+        healt_player.Add(GameObject.Find("PlayerHealthIcon2"));
+
+        healt_enemy.Add(GameObject.Find("EnemyHealthIcon1"));
+        healt_enemy.Add(GameObject.Find("EnemyHealthIcon2"));
+    }
+
+    private void del_health_player()
+    {
+        if (healt_player.Count > 0)
+        {
+            GameObject health = healt_player[0];
+            healt_player.Remove(health);
+            health.transform.position = new Vector3(123f, 123f, 123f);
+        }
+    }
+
+    private void del_health_enemy()
+    {
+        if (healt_enemy.Count > 0)
+        {
+            GameObject health = healt_enemy[0];
+            healt_enemy.Remove(health);
+            health.transform.position = new Vector3(123f, 123f, 123f);
+        }
+    }
+
+    private void check_winner_in_round()
+    {
+        int player_score = this.panel.get_player_total();
+        int enemy_score = this.panel.get_enemy_total();
+
+        if (player_score > enemy_score)
+        {
+            this.del_health_enemy();
+        }
+        else if (player_score == enemy_score)
+        {
+            this.del_health_enemy();
+            this.del_health_player();
+        }
+        else
+        {
+            this.del_health_player();
+        }
+    }
+
+    private void check_winner()
+    {
+        if (this.healt_player.Count == 0 && this.healt_enemy.Count > 0)
+        {
+            this.winner = GameObject.Find("player_ciri").GetComponent<Player>();
+        } 
+        else if (this.healt_enemy.Count == 0 && this.healt_player.Count > 0)
+        {
+            this.winner = GameObject.Find("player_geralt").GetComponent<Player>();
+        }
+    }
 
     private void create_gwent()
     {
@@ -54,32 +122,70 @@ public class GwentGame : MonoBehaviour
         this.round.p1_line_melee.update_score_lines();
     }
 
+    private void create_win(string path)
+    {
+        GameObject okno = new GameObject("WIN");
+        SpriteRenderer render = okno.AddComponent<SpriteRenderer>();
+        Sprite sprite = UnityEditor.AssetDatabase.LoadAssetAtPath<Sprite>(path);
+        render.sprite = sprite;
+        okno.transform.position = new Vector3(0f, 0f, -1f);
+        okno.transform.localScale = new Vector3(1.4f, 1.4f, 1f);
+
+    }
+
     public void create_round()
     {
         if (this.number_round != 0)
         {
+            this.check_winner_in_round();
+            this.check_winner();
             this.del_round(this.round);
         }
-        this.number_round++;
-        GameObject p1 = GameObject.Find("player_geralt");
-        GameObject p2 = GameObject.Find("player_ciri");
-        Player player1 = p1.GetComponent<Player>();
-        Player player2 = p2.GetComponent<Player>();
+        if (healt_player.Count == 0 && healt_enemy.Count == 0)
+        {
+            this.create_win("Assets/Skins/win/draw.png");
+        }
+        else if (this.winner == null)
+        {
+            this.number_round++;
+            GameObject p1 = GameObject.Find("player_geralt");
+            GameObject p2 = GameObject.Find("player_ciri");
+            Player player1 = p1.GetComponent<Player>();
+            Player player2 = p2.GetComponent<Player>();
 
-        GameObject gwent = GameObject.Find("GameField");
-        GameObject _round = new GameObject("round" + this.number_round.ToString());
-        this.round = _round.AddComponent<GwentRound>();
-        this.round.initialization(1, player1, player2);
-        this.round.transform.SetParent(null);
-        this.round.transform.SetParent(gwent.transform);
+            GameObject gwent = GameObject.Find("GameField");
+            GameObject _round = new GameObject("round" + this.number_round.ToString());
+            this.round = _round.AddComponent<GwentRound>();
+            this.round.initialization(1, player1, player2);
+            this.round.transform.SetParent(null);
+            this.round.transform.SetParent(gwent.transform);
 
-        this.panel.set_round(this.round);
+            this.panel.set_round(this.round);
 
-        this.set_score_null();
+            this.set_score_null();
+        }
+        else
+        {
+            if (this.winner.name_ == "geralt")
+            {
+                this.create_win("Assets/Skins/win/win.png");
+            }
+            else 
+            {
+                this.create_win("Assets/Skins/win/los.png");
+            }
+                
+        }
     }
 
     public void end()
     {
+        this.check_winner_in_round();
+        this.check_winner();
+        if (winner == null)
+        {
+            this.create_win("Assets/Skins/win/draw.png");
+        }
         this.del_round(this.round);
     }
 
@@ -92,6 +198,8 @@ public class GwentGame : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        init_health();
+
         find_panel_score();
 
         this.number_round = 0;
--- Assets/GameFieldScripts/RowPointsPanel.cs
+++ Assets/GameFieldScripts/RowPointsPanel.cs
@@ -15,6 +15,15 @@ public class RowPointsPanel : MonoBehaviour
 
     public GwentRound gwentRound;
 
+    public int get_player_total()
+    {
+        return this.gwentRound.p1_line_melee.score + this.gwentRound.p1_line_ranged.score + this.gwentRound.p1_line_siege.score;
+    }
+
+    public int get_enemy_total()
+    {
+        return this.gwentRound.p2_line_melee.score + this.gwentRound.p2_line_ranged.score + this.gwentRound.p2_line_siege.score;
+    }
 
     public void set_round(GwentRound round)
     {
@@ -27,13 +36,13 @@ public class RowPointsPanel : MonoBehaviour
         this.playerRangedPointsText.text = $"{this.gwentRound.p1_line_ranged.score}";
         this.playerSiegePointsText.text = $"{this.gwentRound.p1_line_siege.score}";
 
-        this.player_total.text = $"{this.gwentRound.p1_line_melee.score + this.gwentRound.p1_line_ranged.score + this.gwentRound.p1_line_siege.score}";
+        this.player_total.text = $"{this.get_player_total()}";
 
 
         this.enemyMeleePointsText.text = $"{this.gwentRound.p2_line_melee.score}";
         this.enemyRangedPointsText.text = $"{this.gwentRound.p2_line_ranged.score}";
         this.enemySiegePointsText.text = $"{this.gwentRound.p2_line_siege.score}";
 
-        this.enemy_total.text = $"{this.gwentRound.p2_line_melee.score + this.gwentRound.p2_line_ranged.score + this.gwentRound.p2_line_siege.score}";
+        this.enemy_total.text = $"{this.get_enemy_total()}";
     }
 }
